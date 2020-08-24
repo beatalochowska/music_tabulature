f47ef803bd6a5646569e6d86ded3703e38de1155
beatalochowska <beata.lochowska@gmail.com>
2020-08-24 23:37:23 +0200

    styling result list

--- src/components/ResultsList/ResultsList.tsx
+++ src/components/ResultsList/ResultsList.tsx
@@ -14,34 +14,46 @@ const titleMessage: { [key: string]: string } = {
   SUCCESS: "Searched items list",
 };
 
+const tabulatorsConverter: { [key: string]: string } = {
+  CHORDS: "chords",
+  TEXT_BASS_TAB: "bass",
+  TEXT_GUITAR_TAB: "guitar",
+  PLAYER: "player",
+};
+
 function ResultsList({ list, searchStatus }: ResultsListProps) {
   if (searchStatus === "ERROR") {
     return <p>Sorry, something went wrong :( Please try again later.</p>;
   }
   return (
-    <>
+    <div className={styles.searchResults}>
       {searchStatus === "LOADING" && <div className={styles.spinner}></div>}
-      {searchStatus === "INIT" && <h2>{titleMessage[searchStatus]}</h2>}
-      {searchStatus === "SUCCESS" && <h2>{titleMessage[searchStatus]}</h2>}
-      {list.length === 0 && searchStatus === "SUCCESS" && (
-        <p>No results found</p>
+      {searchStatus === "INIT" && (
+        <h2 className={styles.searchTitle}>{titleMessage[searchStatus]}</h2>
       )}
-      <ul>
-        {list.map(({ artist, songTitle, tabTypes }, id) => (
-          <table key={id}>
-            <tbody>
-              <tr>
-                <td>{artist}</td>
-                <td>{songTitle}</td>
+
+      {list.length === 0 && searchStatus === "SUCCESS" && (
+        <p>Sorry, we didn't find your tabulators :( </p>
+      )}
+
+      <table className={styles.results}>
+        <tbody>
+          {list.map(({ artist, songTitle, tabTypes }, id) => (
+            <tr key={id} className={styles.searchRecord}>
+              <td className={styles.songTitle}>{songTitle}</td>
+              <td className={styles.artist}>{artist}</td>
+              <td className={styles.tabulators}>
                 {tabTypes.map((el, id) => (
-                  <td key={id}>{el}</td>
+                  <button className={styles.tabulator} key={id}>
+                    {tabulatorsConverter[el]}
+                  </button>
                 ))}
-              </tr>
-            </tbody>
-          </table>
-        ))}
-      </ul>
-    </>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
